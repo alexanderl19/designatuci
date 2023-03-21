4583fe1ab2a91959770546ae92875eb8da28e758
Alexander Liu <a@alexanderliu.com>
2023-03-21 07:19:45 -0700

    refactor: text to typescript

---
+++ apps/site/src/lib/components/Text/Text.tsx
@@ -0,0 +1,50 @@
+import type { ReactNode } from "react";
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
+
+import cn from "./Text.module.scss";
+
+interface TextProps {
+  size?: "S" | "L" | "XL" | "XXL" | "XXXL";
+  color?:
+    | "white"
+    | "black"
+    | "gray"
+    | "grey"
+    | "silver"
+    | "blue"
+    | "sky"
+    | "orange"
+    | "yellow"
+    | "pink"
+    | "red"
+    | "violet"
+    | "green";
+  hoverable?: boolean;
+  icon?: boolean | "right";
+  className?: ClassValue | ClassValue[];
+  children?: ReactNode;
+}
+const Text = ({
+  size,
+  color,
+  icon = false,
+  className,
+  children,
+  ...props
+}: TextProps) => (
+  <div
+    className={clsx(
+      cn.container,
+      cn[size],
+      cn[color],
+      { [cn.pagelink]: icon === true || icon === "right" },
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </div>
+);
+
+export default Text;
